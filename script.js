0e65025f1ca34d550ed436cd7b9fc359093aaf51
nvkob1 <nvkob1@gmail.com>
2025-03-02 19:36:42 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Simple FAQ toggle functionality
-document.querySelectorAll('.faq-question').forEach(question => {
-    question.addEventListener('click', () => {
-        const item = question.parentElement;
-        item.classList.toggle('active');
-    });
-});
